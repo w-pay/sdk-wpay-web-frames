ae04ccf060be88779d01d71f4ad4ab23a0f147e6
Ben Farrelly <ben.farrelly@gmail.com>
2021-05-03 11:59:02 +1000

    Tryiong cardinal.trigger for session reset

--- src/services/types/IAuthorzationService.ts
+++
@@ -1,6 +0,0 @@
-export default interface IAuthorizationService {
-    readonly AuthorizationToken: string;
-
-    login(username: string, password: string): Promise<any>;
-    isAuthenticated(): boolean;
-}
