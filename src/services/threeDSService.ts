497ea66b541933c1d53f68e96f15290e45d1c5c0
Ben Farrelly <ben.farrelly@gmail.com>
2021-06-01 10:59:00 +1000

    Changed the way that enrollment check options are passed

--- src/services/threeDSService.ts
+++ src/services/threeDSService.ts
@@ -45,7 +45,7 @@ export default class ThreeDSService implements IThreeDSService {
         return initResponse;
     }
 
-    public async verifyEnrollment(sessionId: string, paymentInstrumentId: string, windowSize?: string): Promise<ValidatePaymentsResponse> {
+    public async verifyEnrollment(sessionId: string, paymentInstrumentId: string, threeDS?: string): Promise<ValidatePaymentsResponse> {
         const response = await fetch(`${this.apiBase}/customer/3ds/session/enrolment`, {
             method: 'POST',
             headers: {
@@ -59,7 +59,7 @@ export default class ThreeDSService implements IThreeDSService {
             body: JSON.stringify({
                 data: {
                     sessionId,
-                    windowSize
+                    options: threeDS
                 }
             })
         });
